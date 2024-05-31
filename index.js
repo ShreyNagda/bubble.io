70fd1a6e0c65df8088b51b06babcc1ed6473c465
Shrey Nagda <shreynagda2714@gmail.com>
2024-06-01 01:34:28 +0530

    URL bug resolution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector("#submit-btn").addEventListener("click", function () {
   var timeVal = document.querySelector(`input[name="time"]:checked`).value;
-  window.location = `/game.html?${timeVal}`;
+  window.location = `bubble.io/game.html?${timeVal}`;
 });
